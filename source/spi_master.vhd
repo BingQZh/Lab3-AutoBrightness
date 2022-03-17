7d2f9918757b1633cfff3a28d94de9e83b6df8b6
Valeriya Svichkar <valeriya17@gmail.com>
2022-03-17 11:28:44 -0700

    Update spi_master.vhd

--- source/spi_master.vhd
+++ source/spi_master.vhd
@@ -4,9 +4,11 @@ use ieee.numeric_std.all;
 
 entity spi_master is
     generic (
-        clk_hz : integer; --FPGA clock
-        total_bits : integer --total bits tx by sensor chip
-        sclk_hz : integer); --sensor’s frequency 
+        clk_hz : integer := 100MHz?; --FPGA clock
+        total_bits : integer := 16; --total bits tx by sensor chip
+        leading_z : integer := 3;
+        trailing_z : integer := 4;
+        sclk_hz : integer := 4MHz?); --sensor’s frequency 
     port (
         --fpga system
         clk : in std_logic;
@@ -22,7 +24,7 @@ entity spi_master is
         -- Internal interface when obtaining data back from slave chip
         ready : in std_logic;
         valid : out std_logic;
-        data : out std_logic_vector(7 downto 0));
+        data : out std_logic_vector(total_bits-leading_z-trailing_z-2 downto 0));
 end spi_master;
 
 architecture rtl of spi_master is
@@ -30,10 +32,10 @@ architecture rtl of spi_master is
     type STATES is (IDLE, TRANSMISSION); -- FSM states
     signal Master_State : STATES := IDLE;
 
-    signal temp_data : std_logic_vector(7 downto 0);
-    signal i : integer range 0 to 7 := 0;
+    signal temp_data : std_logic_vector(total_bits-leading_z-trailing_z-2 downto 0);
+    signal i : integer range 0 to total_bits-leading_z-trailing_z-2 := total_bits-leading_z-trailing_z-2;
 
-    signal sclk_counter : integer range 0 to 16 := 0;
+    signal sclk_counter : integer range 0 to total_bits := 0;
 
     function Rising_Edge_Check(FF2 : std_logic;
             FF3 : std_logic) return std_logic is
@@ -116,6 +118,7 @@ begin
                     when IDLE =>
                         sclk <= '1';
                         prev_sclk <= sclk;
+
                         if ready = '1' then
                             Master_State <= TRANSMISSION;
                         end if;
@@ -124,20 +127,30 @@ begin
                         prev_sclk <= sclk;
                         sclk <= not sclk after sclk_hz / 2;
                         cs <= '0';
+
+                        -- the following if statement checks if data is ready to output!
+                        if i = 0 then
+                            data <= temp_data;
+                            i <= 0;
+                            valid <= '1';
+                        else
+                            valid <= '0';
+                        end if;
+
                         if Rising_Edge_Check(prev_sclk, sclk) = '1' then -- rising edge of B clk
-                            if sckl_counter < 3 then
-                                sckl_counter <= sckl_counter + 1;
+                            if sclk_counter < leading_z then
+                                sclk_counter <= sclk_counter + 1;
                             else    
-                                temp_data(i) <= miso;
-                                sckl_counter <= sckl_counter + 1;
-                                if i < 7 then
-                                    i <= i + 1;
+                                temp_data(i) <= valid_miso;
+                                sclk_counter <= sclk_counter + 1;
+                                if sclk_counter > total_bits-trailing_z-1 then
+                                    i <= i - 1;
                                 else
-                                    if sckl_counter < 16 then
-                                        sckl_counter <= sckl_counter + 1;
+                                    if sclk_counter < total_bits then
+                                        sclk_counter <= sclk_counter + 1;
                                     else
-                                        i <= 0;
                                         sclk_counter <= 0;
+                                        sclk <= '1';
                                         Master_State <= IDLE;
                                     end if;
                                 end if;
